ba339e79154f338a663a276f4fef161bbb52561d
nemo <nemo@MacBook-Pro.local>
2020-09-22 23:14:53 +0800

    feat: useMemo

--- src/memo/UseMemo.js
+++ src/memo/UseMemo.js
@@ -1,41 +1,51 @@
-import React, { useState,memo,useMemo, useCallback } from 'react'
-
-
-function Child({name}){
-  // const memo_getName= useMemo(()=>{
-  //   return ()=>{
-  //     return name
-  //   }
-  // },[name])
-
-  const memo_getName=useCallback(()=>{return name},[name])
-  return <div>{memo_getName()}</div>
-}
-
-
-
+import React,{useState,useMemo} from 'react'
 
 function UseMemo() {
-  const [name,setName]= useState('Jim')
-  const [name2,setName2]= useState('Nemo')
- 
+  const [count, setCount] = useState(10)
+  const [name,setName] = useState('Jim')
+  const memoComputed=useMemo(()=>{
+    let result=1
+    for(let i=count;i>0;i--){
+      result = i*result
+    }
+    //  用useMomo包裹以后 更新name 这个函数不会执行
+
+
+    //StrictMode 在 development mode 下一些 hooks 的回调会调用两次以确保没有副作用。
+    // https://reactjs.org/docs/strict-mode.html#detecting-unexpected-side-effects
+    console.log(result)
+    return result
+   },[count])
+  function computed(){
+    let result=1
+    for(let i=count;i>0;i--){
+      result = i*result
+    }
+    // 如果这样不用useMemo包裹,没吃更新name 这个函数都会执行
+    console.log(result)
+    return result
+  }
   return (
     <div>
-      <Child name={name}/>
-      <Child name={name2}/>
+      {count}
+      <br/>
+       {memoComputed}
+       <br />
+      {computed}
+      <br />
+      {name}
       <button onClick={()=>{
-        setName(name==='Jim'?'Kim':'Jim')
-      }}>Jim-Kim</button>
+        setName('Jim'+new Date().getTime())
+      }}>name+time</button>
       <button onClick={()=>{
-        setName2(name2==='Nemo'?'Marlin':'Nemo')
-      }}>Nemo-Marlin</button>
+        if(count>12){
+          setCount(10)
+          return
+        }
+        setCount(count+1)
+      }}>count++</button>
     </div>
   )
 }
-
-
-
-
-
 
 export default UseMemo
